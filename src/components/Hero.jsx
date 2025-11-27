0a6ef55483130d040e08ad96aec10c084dfba830
krrishmahar <krrishmahar5@gmail.com>
2025-11-27 18:51:27 +0530

    Computer canvas added with ThreeJs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import Computers from "./canvas/Computers";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div className="paddingX absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5">
+        <div className="flex flex-col justify-center mt-5 items-center">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className="heroHeadText text-white">
+            Hi, I'm <span className="text-[#915EFF]">Krrish</span>
+          </h1>
+          <p className="heroSubText text-white-100 mt-2">
+            I develop DevOps solutions that enhance system reliability,&nbsp;
+            <br className="sm:block hidden" />
+            and streamline deployment processes.
+          </p>
+        </div>
+      </div>
+      <Computers />
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+import * as THREE from "three";
+import { Environment } from "@react-three/drei";
 
 const Computers = () => {
+  const computer = useGLTF("/desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      {/* <ambientLight intensity={0.5} /> */}
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.5, -1.5]}
+        rotation={[-0.01, 0.18, -0.1]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true, toneMapping: THREE.NoToneMapping }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Environment preset="studio" />
+        <ambientLight intensity={0.5} />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
